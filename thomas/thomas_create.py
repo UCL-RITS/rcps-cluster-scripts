e75a8ab388bbd62fbab81db4f2ba4713804242f4
Heather Kelly <h.kelly@ucl.ac.uk>
2017-08-22 10:10:28 +0100

    thomas-create can now approve request ids

--- thomas/thomas_create.py
+++ thomas/thomas_create.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 import os.path
+import sys
 import argparse
 import subprocess
 import validate
+import mysql.connector
+from mysql.connector import errorcode
+from tabulate import tabulate
 import thomas_show
 import thomas_add
 
 # This should take all the arguments necessary to run both thomas-add user 
 # and createThomasuser in one. It gets the next available mmm username
 # automatically if this is not a UCL user.
+# It also acts on account requests existing in the Thomas database.
 
 # If a UCL email is given, this should be an active UCL user and their
 # existing username should have been supplied.
@@ -27,25 +32,32 @@ def getargs():
     subparsers = parser.add_subparsers(dest="subcommand")
 
     # Create a user entirely manually and add them to the database
+    # Debug etc are duplicated for each subparser as otherwise that flag
+    # can't be given after the arguments for user
     userparser = subparsers.add_parser("user", help="Create a new active user account and add their info to the Thomas database. Non-UCL users will be given the next free mmm username automatically.")
-    parser.add_argument("-u", "--user", dest="username", help="Existing UCL username")
-    parser.add_argument("-e", "--email", dest="email", help="Institutional email address of user", required=True, action=CheckUCL)
-    parser.add_argument("-n", "--name", dest="given_name", help="Given name of user", required=True)
-    parser.add_argument("-s", "--surname", dest="surname", help="Surname of user")
-    parser.add_argument("-k", "--key", dest='ssh_key', help="User's public ssh key (quotes necessary)", required=True)
-    parser.add_argument("-p", "--project", dest="project_ID", help="Initial project the user belongs to", required=True)
-    parser.add_argument("-c", "--contact", dest="poc_id", help="An existing Point of Contact ID", required=True)
-    parser.add_argument("-b", "--cc", dest="cc_email", help="CC the welcome email to this address")
-    parser.add_argument("--noemail", help="Create account, don't send welcome email", action='store_true')
-    parser.add_argument("--debug", help="Show SQL query submitted without committing the change", action='store_true')
-    parser.add_argument("--nosshverify", help="Do not verify SSH key (use with caution!)", action='store_true')
+    userparser.add_argument("-u", "--user", dest="username", help="Existing UCL username")
+    userparser.add_argument("-e", "--email", dest="email", help="Institutional email address of user", required=True, action=CheckUCL)
+    userparser.add_argument("-n", "--name", dest="given_name", help="Given name of user", required=True)
+    userparser.add_argument("-s", "--surname", dest="surname", help="Surname of user")
+    userparser.add_argument("-k", "--key", dest='ssh_key', help="User's public ssh key (quotes necessary)", required=True)
+    userparser.add_argument("-p", "--project", dest="project_ID", help="Initial project the user belongs to", required=True)
+    userparser.add_argument("-c", "--contact", dest="poc_id", help="An existing Point of Contact ID", required=True)
+    userparser.add_argument("-b", "--cc", dest="cc_email", help="CC the welcome email to this address")
+    userparser.add_argument("--noemail", help="Create account, don't send welcome email", action='store_true')
+    userparser.add_argument("--debug", help="Show SQL query submitted without committing the change", action='store_true')
+    userparser.add_argument("--nosshverify", help="Do not verify SSH key (use with caution!)", action='store_true')
     
     # Used when request(s) exists in the thomas database and we get the input from there
     # Requires at least one id to be provided. request is a list.
-    requestparser = subparsers.add_parser("request", nargs='+', type=int, help="The request id(s) to carry out, divided by spaces")
+    requestparser = subparsers.add_parser("request")
+    requestparser.add_argument("request", nargs='+', type=int, help="The request id(s) to carry out, divided by spaces")
+    requestparser.add_argument("--noemail", help="Create account, don't send welcome email", action='store_true')
+    requestparser.add_argument("--debug", help="Show SQL query submitted without committing the change", action='store_true')
+    requestparser.add_argument("--nosshverify", help="Do not verify SSH key (use with caution!)", action='store_true')
+
 
     # Show the usage if no arguments are supplied
-    if len(argv) < 1:
+    if len(sys.argv[1:]) < 1:
         parser.print_usage()
         exit(1)
 
@@ -82,6 +94,7 @@ def createaccount(args):
     if (args.noemail):
         create_args.append('-n') 
     if (args.debug):
+        print("Arguments that would be used:")
         return print(create_args)
     else:
         return subprocess.check_call(create_args)
@@ -103,10 +116,11 @@ def create_and_add_user(args):
 # end createuser
 
 def updaterequest(args, cursor):
-    query = ("""UPDATE requests SET isdone='1', approver=%(approver)s
-                WHERE id=%(id)s""")
-    cursor.execute(query, args_dict)
-    
+    query = ("""UPDATE requests SET isdone='1', approver=%s 
+                WHERE id=%s""")
+    cursor.execute(query, (args.approver, args.id))
+    if (args.debug):
+        print(cursor.statement)    
 
 def approverequest(args, args_dict):
 
@@ -114,37 +128,35 @@ def approverequest(args, args_dict):
     # (.thomas.cnf has readonly connection details as the default option group)
     try:
         conn = mysql.connector.connect(option_files=os.path.expanduser('~/.thomas.cnf'), option_groups='thomas_update', database='thomas')
-        cursor = conn.cursor()
+        cursor = conn.cursor(dictionary=True)
 
         # get the arguments from the database
         # args.request is a list of ids     
         # make the format string for the number of ids we are checking
         format_strings = ','.join(['%s'] * len(args.request))
-        query = ("""SELECT username, email, ssh_key, poc_cc_email, isdone, approver FROM requests 
+        query = ("""SELECT id, username, email, ssh_key, poc_cc_email, isdone, approver FROM requests 
                     WHERE id IN (%s)""" % format_strings)
         cursor.execute(query, tuple(args.request))
         results = cursor.fetchall()
         if (args.debug):
-            thomas_show.tableprint(cursor, results)
-
+            tableprint(results)
         # check if this request still needs doing
-        for (row in results):
-            if (row.isdone == '0'):
-
+        for row in results:
+            if (row['isdone'] == 0):
                 # set the variables
-                args.username = row.username 
-                args.email = row.email
-                args.ssh_key = row.ssh_key
-                args.cc_email = row.poc_cc_email
-                args.id = row.id
+                args.username = row['username'] 
+                args.email = row['email']
+                args.ssh_key = row['ssh_key']
+                args.cc_email = row['poc_cc_email']
+                args.id = row['id']
                 args.approver = os.environ['USER']
                 # create the account
                 createaccount(args)
                 # update the request status
                 updaterequest(args, cursor)
-                
+               
             else:
-                print("Request id " + row.id + " was already approved by " + row.approver)
+                print("Request id " + str(row['id']) + " was already approved by " + row['approver'])
 
         # commit the change to the database unless we are debugging
         if (not args.debug):
@@ -162,7 +174,10 @@ def approverequest(args, args_dict):
         conn.close()
 # end approverequest    
 
-
+# Write out results as a table with header and separators
+def tableprint(results):
+    print(tabulate(results, headers="keys", tablefmt="psql"))
+    print("")
     
 if __name__ == "__main__":
 
@@ -181,6 +196,5 @@ if __name__ == "__main__":
     elif (args.subcommand == "request"):
         approverequest(args, args_dict)
         
-
 
 # end main
