052e09140c3979dd58872922fd08a062f6260a19
Owain Kenway <o.kenway@ucl.ac.uk>
2018-11-19 14:04:16 +0000

    Updated for newest JSON example:
    1) Some classes can have empty fields, yay.
    2) JSON file can contain multiple tickets.
    3) Detects and warns about unknown fields in JSON file.

--- thomas/safe_json_decoder.py
+++ thomas/safe_json_decoder.py
@@ -11,7 +11,12 @@
 # For reasons the request is enclosed in a Sysadmin object.
 class SysAdmin:
 
+    known_keys = ["Id", "Type", "Status", "StartDate", "EndDate", "Machine", "Handler", "Person", "ProjectGroup", "Project", "Account", "ExtraText"]
+
     def __init__(self, SysAdminDict):
+        for a in SysAdminDict.keys():
+            if a not in self.known_keys:
+                print("Warning [SysAdmin]: Detected unknown key: " + a + ": " + str(SysAdminDict[a]))
         self.Id=SysAdminDict["Id"]
         self.Type=SysAdminDict["Type"]
         self.Status=SysAdminDict["Status"]
@@ -20,10 +25,17 @@ class SysAdmin:
         self.Machine=SysAdminDict["Machine"]
         self.HandlerName=SysAdminDict["Handler"]["Name"]
         self.HandlerEmail=SysAdminDict["Handler"]["Email"]
-        self.Person=Person(SysAdminDict["Person"])
+        self.Person=""
+        if "Person" in SysAdminDict.keys():
+            self.Person=Person(SysAdminDict["Person"])
         self.ProjectGroup=ProjectGroup(SysAdminDict["ProjectGroup"])
         self.Project=Project(SysAdminDict["Project"])
-        self.Account=Account(SysAdminDict["Account"])
+        self.Account=""
+        if "Account" in SysAdminDict.keys():
+            self.Account=Account(SysAdminDict["Account"])            
+        self.ExtraText=""
+        if "ExtraText" in SysAdminDict.keys():
+            self.ExtraText=SysAdminDict["ExtraText"]
 
     def __str__(self):
         return "SysAdmin: " + ",\n".join([self.Id, 
@@ -37,11 +49,17 @@ class SysAdmin:
                                         str(self.Project),
                                         str(self.ProjectGroup),
                                         str(self.Account),
-                                        self.Machine])
+                                        self.Machine,
+                                        self.ExtraText])
 
 class Project:
 
+    known_keys = ["Code", "Name", "Status", "ProjectClass", "FundingBody", "Machines", "TopGroup"]
+
     def __init__(self, ProjectDict):
+        for a in ProjectDict.keys():
+            if a not in self.known_keys:
+                print("Warning [Project]: Detected unknown key: " + a + ": " + str(ProjectDict[a]))
         self.Code=ProjectDict["Code"]
         self.Name=ProjectDict["Name"]
         self.Status=ProjectDict["Status"]
@@ -61,7 +79,12 @@ class Project:
 
 class ProjectGroup:
 
+    known_keys = ["Code", "GroupID"]
+
     def __init__(self, ProjectGroupDict):
+        for a in ProjectGroupDict.keys():
+            if a not in self.known_keys:
+                print("Warning [ProjectGroup]: Detected unknown key: " + a + ": " + str(ProjectGroupDict[a]))
         self.Code=ProjectGroupDict["Code"]
         self.GroupID=ProjectGroupDict["GroupID"]
 
@@ -71,7 +94,14 @@ class ProjectGroup:
 
 class Account:
 
+    known_keys = ["Name", "GID", "Groups", "Person", "UID", "Machines"]
+
     def __init__(self, AccountDict):
+
+        for a in AccountDict.keys():
+            if (a not in self.known_keys) and (not a.startswith("Group")):
+                print("Warning [Account]: Detected unknown key: " + a + ": " + str(AccountDict[a]))
+
         self.Name=AccountDict["Name"]
         self.GroupID=AccountDict["GID"]
         
@@ -99,15 +129,32 @@ class Account:
 
 class Person:
 
+    known_keys = ["Name", "Email", "WebName", "PublicKey", "NormalisedPublicKey", "HartreeName"]
+
     def __init__(self, PersonDict):
+        for a in PersonDict.keys():
+            if a not in self.known_keys:
+                print("Warning [Person]: Detected unknown key: " + a + ": " + str(PersonDict[a]))
+
         self.Title=PersonDict["Name"]["Title"]
+        if type(self.Title) == type(None):
+            self.Title = ""
         self.FirstName=PersonDict["Name"]["Firstname"]
         self.LastName=PersonDict["Name"]["Lastname"]
         self.Email=PersonDict["Email"]
-        self.WebName=PersonDict["WebName"]
-        self.PublicKey=PersonDict["PublicKey"]
-        self.NormalisedPublicKey=PersonDict["NormalisedPublicKey"]
-        self.HartreeName=PersonDict["HartreeName"]
+        self.WebName=""
+        if "WebName" in PersonDict.keys():
+            self.WebName=PersonDict["WebName"]
+        self.PublicKey=""
+        if "PublicKey" in PersonDict.keys():
+            self.PublicKey=PersonDict["PublicKey"]
+        self.NormalisedPublicKey=""
+        if "NormalisedPublicKey" in PersonDict.keys():
+            self.NormalisedPublicKey=PersonDict["NormalisedPublicKey"]
+        self.HartreeName=""
+        if "HartreeName" in PersonDict.keys():
+            self.HartreeName=PersonDict["HartreeName"]
+
 
     def __str__(self):
         return "Person: " + ",\n".join([
@@ -129,7 +176,15 @@ class AccountRequest:
 def JSONtoAccountRequest(JSONData):
     import json
     from io import StringIO
-    return AccountRequest(json.load(StringIO(JSONData)))
+    Tickets = []
+    jsdata = json.load(StringIO(JSONData))
+    if type(jsdata) == list:
+        for a in jsdata:
+            Tickets.append(AccountRequest(a))
+    else:
+        Tickets.append(AccountRequest(jsdata))
+
+    return Tickets
 
 # If this is run directly, process test.json in the current working directory and print the output as a string.
 if __name__=="__main__":
@@ -145,5 +200,7 @@ if __name__=="__main__":
     jdata=f.read()
     ar=JSONtoAccountRequest(jdata)
     f.close()
-    print(str(ar.Ticket))
+    for a in ar:
+        print(str(a.Ticket))
     
+    print("Number of tickets included: " + str(len(ar)))
