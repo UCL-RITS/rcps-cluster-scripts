c4a247627fde8f8d5ceba9abdaa88ff3cfd493bd
heatherkellyucl <h.kelly@ucl.ac.uk>
2018-12-11 16:47:53 +0000

    Started adding safetickets database functions

--- thomas/safe_tickets.py
+++ thomas/safe_tickets.py
@@ -14,13 +14,15 @@ import requests
 import safe_json_decoder as decoder
 #from ldap3 import Server, Connection, ALL
 #import socket
-#import thomas_queries
+import thomas_queries
 #import thomas_utils
 
 def getargs(argv):
     parser = argparse.ArgumentParser(description="Show or update and close tickets from SAFE. Use [positional argument -h] for more help.")
     parser.add_argument("-s", "--show", dest="show", help="Show all current open tickets", action='store_true')
     parser.add_argument("-f", "--file", dest="jsonfile", default=None, help="Parse json tickets from a file")
+    parser.add_argument("-r", "--refresh", dest="refresh", help="Refresh open tickets in DB", action='store_true')
+    parser.add_argument("-c", "--complete", dest="complete", default=None, help="Complete this ticket ID")
 
     # Show the usage if no arguments are supplied
     if len(argv) < 1:
@@ -54,6 +56,14 @@ def getopentickets(config):
         print("Request not successful, code " + str(request.status_code))
 
 # end getopentickets
+
+def gettickets(config):
+        # get SAFE tickets
+        jsontickets = getopentickets(config)
+        # parse SAFE tickets
+        ticketlist = decoder.JSONDataToTickets(jsontickets)
+        return ticketlist
+# end gettickets
 
 def updatebudget(ticket_id, projectname):
     parameters = {'qtid':ticket_id, 'new_username':projectname, 'mode':'completed'}
@@ -119,10 +129,7 @@ def main(argv):
     # Show tickets live from SAFE
     if args.show:
         # get SAFE tickets
-        jsontickets = getopentickets(config)
-
-        # parse SAFE tickets
-        ticketlist = decoder.JSONDataToTickets(jsontickets)
+        ticketlist = gettickets(config)
 
         # print SAFE tickets
         for t in ticketlist:
@@ -132,8 +139,42 @@ def main(argv):
         print("Number of tickets included: " + str(len(ticketlist)))
 
 
-    # act on SAFE tickets
+    # these options require a database connection
+    if args.refresh or args.update != None:
+            try:
+                conn = mysql.connector.connect(option_files=os.path.expanduser('~/.thomas.cnf'), option_groups='thomas_update', database='thomas')
+                cursor = conn.cursor()
 
+                # Refresh the database tickets
+                if args.refresh:
+                    # get SAFE tickets as list of dicts
+                    ticketdicts = ticketstodicts(gettickets(config))
+                    # refresh tickets in database
+                    for t in ticketdicts:
+                        cursor.execute(thomas_queries.refreshsafetickets(), t)
+                    # show database tickets
+    
+                # act on SAFE tickets
+
+
+                # commit the change to the database unless we are debugging
+                if not args.debug:
+                    #if args.verbose:
+                    #    print("")
+                    #    print("Committing database change")
+                    #    print("")
+                    conn.commit()
+
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Access denied: Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+            cursor.close()
+            conn.close()
 # end main
 
 # When not imported, use the normal global arguments
