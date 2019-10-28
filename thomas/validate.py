6cd76403a06ee9d7d5cfecb5e1a740afc359c194
heatherkellyucl <h.kelly@ucl.ac.uk>
2019-10-28 14:12:54 +0000

    Need to print as string

--- thomas/validate.py
+++ thomas/validate.py
@@ -56,8 +56,8 @@ def mmm_username_in_range(username):
     if username.startswith(prefix):
         number = int(username[len(prefix):])
         if number > MAX_ACCOUNT_NO:
-            print("Username "+username+ "does not exist. The last existing MMM account is " + MAX_ACCOUNT_NO)
+            print("Username "+username+ "does not exist. The last existing MMM account is " + str(MAX_ACCOUNT_NO))
             exit(1)
         elif number > MAX_ACCOUNT_NO-100:
-            print("WARNING: last existing MMM role account is " + MAX_ACCOUNT_NO + ", we need to request more from ISD.User Services.")
+            print("WARNING: last existing MMM role account is " + str(MAX_ACCOUNT_NO) + ", we need to request more from ISD.User Services.")
 
